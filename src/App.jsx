10f63cf6b5e0d871cab931390ee29beb34bea595
Brian <briandave771@gmail.com>
2023-01-03 23:58:49 +0300

    Create container folder for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import StartQuiz from "./container/StartQuiz";
+import Home from "./container/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <StartQuiz />
+    <Home />
+    
+
+  </div>;
 }
 
 export default App;
---
+++ src/container/StartQuiz.jsx
@@ -0,0 +1,9 @@
+export default function StartQuiz() {
+  return (
+    <div>
+      <h1>Quizzical</h1>
+      <p>Some description if needed</p>
+      <button>Sart quiz</button>
+    </div>
+  );
+}
