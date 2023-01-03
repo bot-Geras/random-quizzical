b4c14f1800e40dfda9f04306b4e747d19ab83bbb
Brian <briandave771@gmail.com>
2023-01-04 00:59:05 +0300

    Add tailwind config

--- src/container/StartQuiz.jsx
+++ src/container/StartQuiz.jsx
@@ -1,10 +1,10 @@
 import {Link} from "react-router-dom"
 export default function StartQuiz() {
   return (
-    <div>
-      <h1>Quizzical</h1>
-      <p>Some description if needed</p>
-      <Link to="home"><button>Start quiz</button></Link>
+    <div className="text-blue-900 h-screen tracking-widest flex flex-col justify-center items-center">
+      <h1 className="font-black text-3xl">Quizzical</h1>
+      <p className="text-blue-00 mt-2">Some description if needed</p>
+      <Link to="home"><button className="mt-8 px-16 py-4 bg-blue-900 rounded-lg text-white">Start quiz</button></Link>
     </div>
   );
 }
