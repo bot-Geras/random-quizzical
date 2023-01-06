f10219d17d8fdff94b5de5501815edb4cb931dc0
Brian <briandave771@gmail.com>
2023-01-06 23:59:23 +0300

    Add relevant elements

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,7 +1,17 @@
 export default function Home() {
     return (
         <div>
-            
+            <div>
+            <p>What is Html?</p>
+            <button className="choice">Hyper</button>
+            <button className="choice">Hyper Text Markup Language</button>
+            <button className="choice">HTML</button>
+            <button className="choice">html</button>
+            </div>
+            <div>
+                <span>1/10</span>
+                <span>2 questions left</span>
+            </div>
         </div>
     )
 }
--- src/container/StartQuiz.jsx
+++ src/container/StartQuiz.jsx
@@ -4,7 +4,7 @@ export default function StartQuiz() {
     <div className="text-blue-900 h-screen tracking-widest flex flex-col justify-center items-center">
       <h1 className="font-black text-3xl">Quizzical</h1>
       <p className="text-blue-00 mt-2">Some description if needed</p>
-      <Link to="home"><button className="mt-8 px-16 py-4 bg-blue-900 rounded-lg text-white">Start quiz</button></Link>
+      <Link to="home"><button className="mt-8 px-16 py-4 bg-blue-900 rounded-lg text-white">Start Quiz</button></Link>
     </div>
   );
 }
